ba6d9a66d85494f5a71f5d6c2457999cdbd042ea
seojungood <seojungood@gmail.com>
2022-10-31 18:20:42 -0600

    Implemented Navbar with home and login page links

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.css';
+import { Navbar } from './components/navbar';
 import { Login } from './pages/login';
 import { Main } from './pages/main';
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <Router>
+        <Navbar />
         <Routes>
           <Route path='/' element={<Main />}/>
           <Route path='/login' element={<Login />}/>
