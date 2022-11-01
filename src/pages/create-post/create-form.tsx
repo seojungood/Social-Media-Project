d95885f9414c5442abb30006baff3094d9b24d5a
seojungood <seojungood@gmail.com>
2022-10-31 21:55:10 -0600

    Implemented functionality of adding post to database. Added minor styling for creating post. Will add more css later for multiple sizing. Also need a functionality to add the posts to main page.

--- src/pages/create-post/create-form.tsx
+++ src/pages/create-post/create-form.tsx
@@ -1,6 +1,10 @@
 import {useForm} from 'react-hook-form';
 import * as yup from 'yup';
 import {yupResolver} from '@hookform/resolvers/yup';
+import {addDoc, collection} from 'firebase/firestore';
+import {db, auth} from '../../config/firebase';
+import {useAuthState} from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
 
 // Interface to specify types of input for TS
 interface CreateFormData {
@@ -9,6 +13,11 @@ interface CreateFormData {
 }
 
 export const CreateForm = () =>{
+  // Grabs user information from FB
+  const [user] = useAuthState(auth);
+
+  const navigate = useNavigate();
+
 
   // Schema specifying title and description content format
   const schema = yup.object().shape({
@@ -22,9 +31,18 @@ export const CreateForm = () =>{
     resolver: yupResolver(schema),
   });
 
+  // Reference to collection of posts in db
+  const postsRef = collection(db, "posts");
+
   // Called when form submitted
-  const onCreatePost = (data: CreateFormData) =>{
-    console.log(data);
+  const onCreatePost = async (data: CreateFormData) =>{
+    await addDoc(postsRef, {
+      ...data,
+      username: user?.displayName,
+      userId: user?.uid,
+    })
+    //Navigate back to homepage
+    navigate('/');
   };
 
   return (
@@ -32,7 +50,7 @@ export const CreateForm = () =>{
       <input placeholder='Title...' {...register("title")}/>
       <p style={{color: "red"}}>{errors.title?.message}</p>
       <textarea placeholder='Description...'{...register("description")}/>
-      <input type="submit" />
+      <input type="submit" className='submitForm' />
     </form>
   );
 };
