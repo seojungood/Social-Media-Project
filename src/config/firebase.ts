0ebb323f379792cad9819ceb04644cf344fcf62a
seojungood <seojungood@gmail.com>
2022-10-31 18:42:52 -0600

    Implemented firebase for login authentication

---
+++ src/config/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC6lUruymRFx_5nPvTlm_yhHOriIDP_OXs",
+  authDomain: "seojungood-project1.firebaseapp.com",
+  projectId: "seojungood-project1",
+  storageBucket: "seojungood-project1.appspot.com",
+  messagingSenderId: "791956520207",
+  appId: "1:791956520207:web:8a2d2e1a9b1f723f5c527a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
