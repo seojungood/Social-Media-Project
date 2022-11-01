f699e635f9f8548ec2a21c4835ec5517c7c2dea4
seojungood <seojungood@gmail.com>
2022-10-31 19:18:47 -0600

    Implemented signout through firebase and minor css for now. Will modify css more later.

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,10 +1,33 @@
 import { Link } from "react-router-dom";
+import { auth } from '../config/firebase';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import {signOut} from 'firebase/auth';
 
 export const Navbar = () =>{
+  const [user] = useAuthState(auth);
+
+  // Function to signout from current user
+  const signUserOut = async () =>{
+    await signOut(auth);
+  }
+
   return (
-    <div>
-      <Link to="/"> Home </Link>
-      <Link to="/login"> Login </Link>
+    <div className="navbar">
+      <div className="links">
+        <Link to="/"> Home </Link>
+        <Link to="/login"> Login </Link>
+      </div>
+    {/* Display Profile name and picture */}
+      <div className="user">
+        {/* Only display when logged in */}
+        {user &&(
+            <>
+              <p>{user?.displayName} </p>
+              <img src={user?.photoURL || ""} />
+              <button onClick={signUserOut}> Log Out </button>
+            </>
+          )}
+      </div>
     </div>
   );
 }
