1c763a0e24f74b12ec1d83ef8920f559e2dc2e88
seojungood <seojungood@gmail.com>
2022-10-31 18:15:33 -0600

    Created main page, nothing in main page yet. Also imported react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.css';
+import { Main } from './pages/main';
 
 function App() {
   return (
     <div className="App">
-
+      <Router>
+        <Routes>
+          <Route path='/' element={<Main />}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
