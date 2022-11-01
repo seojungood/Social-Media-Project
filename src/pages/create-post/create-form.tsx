b724b57772636fc262cfe85e8db6a47e6df85ed8
seojungood <seojungood@gmail.com>
2022-10-31 21:11:32 -0600

    Implemented yup schema, resolver for form, method to be handled on submit, interface for TS.

--- src/pages/create-post/create-form.tsx
+++ src/pages/create-post/create-form.tsx
@@ -1,10 +1,38 @@
 import {useForm} from 'react-hook-form';
 import * as yup from 'yup';
+import {yupResolver} from '@hookform/resolvers/yup';
+
+// Interface to specify types of input for TS
+interface CreateFormData {
+  title: string;
+  description: string;
+}
 
 export const CreateForm = () =>{
-  return (
-    <form>
 
+  // Schema specifying title and description content format
+  const schema = yup.object().shape({
+    title: yup.string().required("Title Required"),
+    description: yup.string(),
+  });
+
+  // Register using the schema created above
+  // handleSubmit to be called when form is submitted
+  const {register, handleSubmit, formState:{errors}} = useForm<CreateFormData>({
+    resolver: yupResolver(schema),
+  });
+
+  // Called when form submitted
+  const onCreatePost = (data: CreateFormData) =>{
+    console.log(data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onCreatePost)}>
+      <input placeholder='Title...' {...register("title")}/>
+      <p style={{color: "red"}}>{errors.title?.message}</p>
+      <textarea placeholder='Description...'{...register("description")}/>
+      <input type="submit" />
     </form>
   );
 };
