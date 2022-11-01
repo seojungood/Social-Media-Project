e5875b23c76713f3429799ab8a1a43583462a899
seojungood <seojungood@gmail.com>
2022-10-31 20:57:36 -0600

    Imported react-hook-form yup and form-resolver. Created some folders for file management

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.css';
 import { Navbar } from './components/navbar';
-import { CreatePost } from './pages/create-post';
+import { CreatePost } from './pages/create-post/create-post';
 import { Login } from './pages/login';
 import { Main } from './pages/main';
 
--- src/pages/create-post.tsx
+++
@@ -1,5 +0,0 @@
-export const CreatePost = () =>{
-  return (
-    <div>Create Post</div>
-  );
-}
