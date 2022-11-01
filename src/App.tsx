42dc5e6d909d789a7c4962a317cc313908bc8f84
seojungood <seojungood@gmail.com>
2022-10-31 18:17:33 -0600

    Added login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.css';
+import { Login } from './pages/login';
 import { Main } from './pages/main';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Router>
         <Routes>
           <Route path='/' element={<Main />}/>
+          <Route path='/login' element={<Login />}/>
         </Routes>
       </Router>
     </div>
