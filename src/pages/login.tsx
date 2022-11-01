100b8c15d84ca0830f6551d1fe91a8eee5e9cbd0
seojungood <seojungood@gmail.com>
2022-10-31 18:58:04 -0600

    Added signing in method using popup window and navigation to home page upon signing in

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,5 +1,22 @@
+import {auth, provider} from '../config/firebase';
+import {signInWithPopup} from 'firebase/auth';
+import {useNavigate}  from 'react-router-dom';
+
 export const Login = () =>{
+
+  const navigate = useNavigate();
+
+  // Sign in with google on popup window
+  const signInWithGoogle = async () =>{
+    const result = await signInWithPopup(auth, provider);
+    // Once logged in, redirected to home page
+    navigate('/');
+  }
+
   return (
-    <div>Login Page</div>
+    <div>
+      <p> Sign In With Google To Continue</p>
+      <button onClick={signInWithGoogle}> Sign In With Google </button>
+    </div>
   );
 }
