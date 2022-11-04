3a5fcfbf6017865b319ac386b9cfa10766ed1690
seojungood <seojungood@gmail.com>
2022-11-03 21:39:18 -0600

    Implemented way of liking and showing the amount of likes. will then implement a way to unlike if like button is clicked again

--- src/pages/main/post.tsx
+++ src/pages/main/post.tsx
@@ -1,4 +1,5 @@
-import { addDoc, collection } from 'firebase/firestore';
+import { addDoc, getDocs, collection, query, where } from 'firebase/firestore';
+import { useEffect, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth, db } from '../../config/firebase';
 import { Post as IPost} from './main';
@@ -11,13 +12,28 @@ export const Post = (props: Props)=>{
   const{post} = props;
   const [user] = useAuthState(auth);
 
-    // Reference to collection of posts in db
+  const [likeAmount, setLikeAmount] = useState<number | null>(null);
+
+    // Reference to collection of likes in db
     const likesRef = collection(db, "likes");
+
+    // gets doc of ids that liked the post 
+    const likesDoc = query(likesRef, where("postId", "==", post.id));
+
+    // Gets all docs with likesDoc 
+    const getLikes = async () =>{
+      const data = await getDocs(likesDoc);
+      setLikeAmount(data.docs.length); 
+    }
 
     // Called when post is liked
     const addLike = async () =>{
       await addDoc(likesRef, {userId: user?.uid, postId: post.id});
     };
+
+    useEffect(()=>{
+      getLikes();
+    },[]);
 
     
   return (
@@ -32,6 +48,7 @@ export const Post = (props: Props)=>{
       <div className='footer'>
         <p> @{post.username}</p>
         <button onClick={addLike}> &#9825; </button>
+        {likeAmount && <p> Likes: {likeAmount} </p>}
       </div>
     </div>
   );
