c3174393c4b6b421dcd1b3d880d59efdbec13585
seojungood <seojungood@gmail.com>
2022-11-03 19:10:30 -0600

    Added post.tsx for main page posts. Will map through postsList in main to map all posts in main page

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -1,5 +1,5 @@
 import {getDocs, collection} from 'firebase/firestore';
-import {db} from '../config/firebase';
+import {db} from '../../config/firebase';
 import { useEffect, useState } from 'react';
 
 interface Post {
