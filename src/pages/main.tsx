4d1100daf1aa588f057cad95167d69c8b61fd845
seojungood <seojungood@gmail.com>
2022-11-03 19:07:12 -0600

    Implemented code for main to be able to show posts that have been submitted by users. Will then create a separate file to format how each post will show in main page

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,5 +1,33 @@
+import {getDocs, collection} from 'firebase/firestore';
+import {db} from '../config/firebase';
+import { useEffect, useState } from 'react';
+
+interface Post {
+  id: string;
+  userId: string;
+  title: string;
+  username: string;
+  description: string;
+}
+
 export const Main = () =>{
+  const [postsList, setPostsList] = useState<Post[] | null >(null);
+  const postsRef = collection(db, "posts");
+
+  const getPosts = async () =>{
+    const data = await getDocs(postsRef);
+    //Set PostsList as array of info from data.doc 
+    setPostsList(data.docs.map((doc)=>({
+      ...doc.data(), id: doc.id
+    })) as Post[] );
+  };
+
+  useEffect(()=>{
+    getPosts();
+  }, []);
+
   return (
     <div>Home Page</div>
   );
-}
+};
+
